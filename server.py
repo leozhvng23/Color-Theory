9e2e3458a47b070d2e4099ef6689f2ae39451f52
leozhvng23 <57277493+leozhvng23@users.noreply.github.com>
2022-04-15 15:28:14 -0400

    added main.css

--- server.py
+++ server.py
@@ -7,7 +7,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def display_home():
-    return render_template('home.html') 
+    return render_template('home.html')
+
+@app.route('/learn')
+def display_learn():
+    return render_template('learn.html')  
+
+@app.route('/quiz')
+def display_quiz():
+    return render_template('quiz.html') 
 
 if __name__ == '__main__':
    app.run(debug = True)
