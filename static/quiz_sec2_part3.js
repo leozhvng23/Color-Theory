30ff0aadab010a85801264b22a3622d7c3430758
Kitty ✨ <kitty@KittydeMacBook-Pro.local>
2022-04-25 14:14:44 -0400

    quiz section2 part2&3

--- static/quiz_sec2_part3.js
+++ static/quiz_sec2_part3.js
@@ -1,3 +1,5 @@
+let num_dropped = 0
+let dropped_color = []
 $(document).ready(function(){
     let container = $("#interactive");
     let c1 = $("<div class='col-6'></div>")
@@ -14,10 +16,57 @@ $(document).ready(function(){
         let c3 = $("<div class='col-4'></div>");
         let circle = $("<div class='color_circle'></div>");
         circle.attr("style","background:"+color+";");
+        circle.attr("data-color",color);
+        $(circle).draggable({
+            cursor: "move",
+            revert: true,
+            stack: ".color_circle",
+            helper: "clone",
+        })
+        $(circle).mouseover(function() {
+            $(this).addClass("circle-on-hover")
+        })
+        $(circle).mouseout(function() {
+            $(this).removeClass("circle-on-hover")
+        })
+
         c3.append(circle);
         r2.append(c3);
     });
     let r3 = $("<div class='row'></div>");
     c2.append(r3);
     r3.append("<div class='col-4 mid_drag_circle'>drop three analogus colors here</div>");
+    $(".mid_drag_circle").droppable({
+        tolerance: "fit",
+        accept: ".color_circle",
+        drop: function(event, ui) {
+                let temp = $(ui.helper).clone();
+                $(this).append(temp);
+                num_dropped += 1
+                dropped_color.push(ui.helper[0].dataset.color);
+                if (num_dropped == 3) {
+                    $(this).droppable('disable');
+                    let data = {};
+                    data['section'] = ans_section;
+                    data['answer'] = dropped_color
+                    console.log(data['answer']);
+                    $.ajax({
+                        type: "POST",
+                        url: "../../../update_ans",                
+                        dataType : "json",
+                        contentType: "application/json; charset=utf-8",
+                        data : JSON.stringify(data),
+                        success: function(result){
+                            $('span.right_footnote').html("Score: "+result.data["score"]+"/10");
+                        },
+                        error: function(request, status, error){
+                            console.log("Error");
+                            console.log(request)
+                            console.log(status)
+                            console.log(error)
+                        }
+                    })
+            }
+        }
+    });
 })
