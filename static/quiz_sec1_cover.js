3daed1fd46049886954f087a07667e9154db8ae7
jchenlukas <jiyaochen12@gmail.com>
2022-04-25 15:04:03 -0400

    cover 1 interaction and style

--- static/quiz_sec1_cover.js
+++ static/quiz_sec1_cover.js
@@ -6,13 +6,44 @@ $(document).ready(function(){
     let palette = $("<div class = 'color-circle main-palette-circle palette-circle palette-circle-bigger'></div>")
 
     palette.append("<div class = 'learn-color-name'>Drop any</div>")
-    palette.append("<div class = 'learn-color-name'>two colors here</div>")
+    palette.append("<div class = 'learn-color-name'>color here</div>")
+
+    $(palette).droppable({
+        tolerance: "fit",
+        accept: ".color-circle",
+        drop: function(event, ui) {
+            palette.empty()
+            palette.append("<div class = 'learn-color-name'>Drop any</div>")
+            palette.append("<div class = 'learn-color-name'>color here</div>")
+            // palette.append("<div class = 'learn-color-name'>color here</div>")
+            let value = ui.draggable.attr("class").split(" ")[1]
+            palette.append($("<div class = 'color-circle "+value+"' style='background: "+value+";'></div>"))
+        }
+    });
 
     $(".center-image-left").append(palette)
 
     let colors_list = ["#FF0000","#0000FF","#FFFF00"]
+    // $.each(colors_list, function(index, value) {
+    //     let color_circle = "<div class = 'color-circle' style='background: "+value+";'></div>"
+    //     $(".center-image-right").append(color_circle)
+    // })
     $.each(colors_list, function(index, value) {
-        let color_circle = "<div class = 'color-circle' style='background: "+value+";'></div>"
+        let color_circle = $("<div class = 'color-circle "+value+"' style='background: "+value+";'></div>")
+
+        $(color_circle).draggable({
+            cursor: "move",
+            revert: true,
+            stack: ".color-circle",
+            helper: "clone",
+        })
+        $(color_circle).mouseover(function() {
+            $(this).addClass("circle-on-hover")
+        })
+        $(color_circle).mouseout(function() {
+            $(this).removeClass("circle-on-hover")
+        })
+
         $(".center-image-right").append(color_circle)
     })
 })
